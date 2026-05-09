74f72063e8247dae2f4a07ccebdd1446988eb92d
Preston <prestonmeek6@gmail.com>
2026-05-09 10:06:14 +0200

    Add coroutine

---
+++ coroutines/main.cpp
@@ -0,0 +1,90 @@
+#include <coroutine>
+#include <optional>
+#include <print>
+#include <thread>
+
+struct Timer {
+public:
+  struct Awaitable {
+    Timer &tmr;
+    std::coroutine_handle<> handle;
+
+    bool await_ready() const noexcept { return false; }
+
+    void await_suspend(std::coroutine_handle<> suspended) noexcept {
+      tmr.awaiter_ = this;
+      handle = suspended;
+    }
+
+    void await_resume() const noexcept {}
+  };
+
+  Awaitable operator co_await() { return Awaitable{*this}; }
+
+  void tick() const {
+    if (awaiter_) {
+      awaiter_->handle.resume();
+    }
+  }
+
+private:
+  // TODO: make linked-list/vector
+  Awaitable *awaiter_;
+};
+
+template <typename T> struct Task {
+public:
+  struct promise_type;
+
+  Task(std::coroutine_handle<promise_type> coro) : coro_{coro} {}
+  ~Task() { coro_.destroy(); }
+
+  void start() { coro_.resume(); }
+
+  std::optional<T> result() const { return coro_.promise().result; }
+
+private:
+  std::coroutine_handle<promise_type> coro_;
+};
+
+template <typename T> struct Task<T>::promise_type {
+  std::optional<T> result;
+
+  Task get_return_object() {
+    return {std::coroutine_handle<promise_type>::from_promise(*this)};
+  }
+
+  auto unhandled_exception() {}
+
+  void return_value(T &&value) { result.emplace(std::move(value)); }
+
+  std::suspend_always initial_suspend() noexcept { return {}; }
+  std::suspend_always final_suspend() noexcept { return {}; }
+};
+
+// Task<int> chained() {
+//   auto awaiter = Awaitable{};
+//   std::println("Before chained suspend");
+//   co_await awaiter;
+//   std::println("After chained suspend");
+//   co_return 69;
+// }
+
+Task<int> foo(Timer &timer) {
+  std::println("Before suspend");
+  co_await timer;
+  std::println("After suspend");
+  co_return 42;
+}
+
+int main() {
+  auto timer = Timer{};
+  auto task_handle = foo(timer);
+
+  task_handle.start();
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  timer.tick();
+
+  std::println("Result: {}", task_handle.result().value_or(-1));
+}
